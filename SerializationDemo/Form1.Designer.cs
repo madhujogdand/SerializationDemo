f09648f120cce4dc6b279af7a6e3b08c53f74251
madhujogdand <gore.madhu03@gmail.com>
2024-05-10 10:56:25 +0530

    Add project files.

---
+++ SerializationDemo/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SerializationDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtPercentage = new System.Windows.Forms.TextBox();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.btnBinaryWrite = new System.Windows.Forms.Button();
+            this.btnBinaryRead = new System.Windows.Forms.Button();
+            this.btnXMLWrite = new System.Windows.Forms.Button();
+            this.btnXMLRead = new System.Windows.Forms.Button();
+            this.btnSOAPWrite = new System.Windows.Forms.Button();
+            this.btnSOAPRead = new System.Windows.Forms.Button();
+            this.btnJSONWrite = new System.Windows.Forms.Button();
+            this.btnJSONRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(44, 42);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(84, 20);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Student Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(189, 41);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 26);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(189, 93);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(44, 93);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(112, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Student Name";
+            // 
+            // txtPercentage
+            // 
+            this.txtPercentage.Location = new System.Drawing.Point(189, 150);
+            this.txtPercentage.Name = "txtPercentage";
+            this.txtPercentage.Size = new System.Drawing.Size(100, 26);
+            this.txtPercentage.TabIndex = 5;
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(44, 150);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(91, 20);
+            this.lblPercentage.TabIndex = 4;
+            this.lblPercentage.Text = "Percentage";
+            // 
+            // btnBinaryWrite
+            // 
+            this.btnBinaryWrite.Location = new System.Drawing.Point(337, 41);
+            this.btnBinaryWrite.Name = "btnBinaryWrite";
+            this.btnBinaryWrite.Size = new System.Drawing.Size(157, 48);
+            this.btnBinaryWrite.TabIndex = 6;
+            this.btnBinaryWrite.Text = "Binary Write";
+            this.btnBinaryWrite.UseVisualStyleBackColor = true;
+            this.btnBinaryWrite.Click += new System.EventHandler(this.btnBinaryWrite_Click);
+            // 
+            // btnBinaryRead
+            // 
+            this.btnBinaryRead.Location = new System.Drawing.Point(548, 44);
+            this.btnBinaryRead.Name = "btnBinaryRead";
+            this.btnBinaryRead.Size = new System.Drawing.Size(132, 45);
+            this.btnBinaryRead.TabIndex = 7;
+            this.btnBinaryRead.Text = "Binary Read";
+            this.btnBinaryRead.UseVisualStyleBackColor = true;
+            this.btnBinaryRead.Click += new System.EventHandler(this.btnBinaryRead_Click);
+            // 
+            // btnXMLWrite
+            // 
+            this.btnXMLWrite.Location = new System.Drawing.Point(337, 116);
+            this.btnXMLWrite.Name = "btnXMLWrite";
+            this.btnXMLWrite.Size = new System.Drawing.Size(157, 38);
+            this.btnXMLWrite.TabIndex = 8;
+            this.btnXMLWrite.Text = "XML Write";
+            this.btnXMLWrite.UseVisualStyleBackColor = true;
+            this.btnXMLWrite.Click += new System.EventHandler(this.btnXMLWrite_Click);
+            // 
+            // btnXMLRead
+            // 
+            this.btnXMLRead.Location = new System.Drawing.Point(548, 116);
+            this.btnXMLRead.Name = "btnXMLRead";
+            this.btnXMLRead.Size = new System.Drawing.Size(132, 38);
+            this.btnXMLRead.TabIndex = 9;
+            this.btnXMLRead.Text = "XML Read";
+            this.btnXMLRead.UseVisualStyleBackColor = true;
+            this.btnXMLRead.Click += new System.EventHandler(this.btnXMLRead_Click);
+            // 
+            // btnSOAPWrite
+            // 
+            this.btnSOAPWrite.Location = new System.Drawing.Point(337, 181);
+            this.btnSOAPWrite.Name = "btnSOAPWrite";
+            this.btnSOAPWrite.Size = new System.Drawing.Size(157, 37);
+            this.btnSOAPWrite.TabIndex = 10;
+            this.btnSOAPWrite.Text = "Soap Write";
+            this.btnSOAPWrite.UseVisualStyleBackColor = true;
+            this.btnSOAPWrite.Click += new System.EventHandler(this.btnSOAPWrite_Click);
+            // 
+            // btnSOAPRead
+            // 
+            this.btnSOAPRead.Location = new System.Drawing.Point(548, 181);
+            this.btnSOAPRead.Name = "btnSOAPRead";
+            this.btnSOAPRead.Size = new System.Drawing.Size(132, 37);
+            this.btnSOAPRead.TabIndex = 11;
+            this.btnSOAPRead.Text = "Soap Read";
+            this.btnSOAPRead.UseVisualStyleBackColor = true;
+            this.btnSOAPRead.Click += new System.EventHandler(this.btnSOAPRead_Click);
+            // 
+            // btnJSONWrite
+            // 
+            this.btnJSONWrite.Location = new System.Drawing.Point(337, 257);
+            this.btnJSONWrite.Name = "btnJSONWrite";
+            this.btnJSONWrite.Size = new System.Drawing.Size(157, 38);
+            this.btnJSONWrite.TabIndex = 12;
+            this.btnJSONWrite.Text = "JSON Write";
+            this.btnJSONWrite.UseVisualStyleBackColor = true;
+            this.btnJSONWrite.Click += new System.EventHandler(this.btnJSONWrite_Click);
+            // 
+            // btnJSONRead
+            // 
+            this.btnJSONRead.Location = new System.Drawing.Point(548, 257);
+            this.btnJSONRead.Name = "btnJSONRead";
+            this.btnJSONRead.Size = new System.Drawing.Size(132, 38);
+            this.btnJSONRead.TabIndex = 13;
+            this.btnJSONRead.Text = "JSON Read";
+            this.btnJSONRead.UseVisualStyleBackColor = true;
+            this.btnJSONRead.Click += new System.EventHandler(this.btnJSONRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnJSONRead);
+            this.Controls.Add(this.btnJSONWrite);
+            this.Controls.Add(this.btnSOAPRead);
+            this.Controls.Add(this.btnSOAPWrite);
+            this.Controls.Add(this.btnXMLRead);
+            this.Controls.Add(this.btnXMLWrite);
+            this.Controls.Add(this.btnBinaryRead);
+            this.Controls.Add(this.btnBinaryWrite);
+            this.Controls.Add(this.txtPercentage);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtPercentage;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.Button btnBinaryWrite;
+        private System.Windows.Forms.Button btnBinaryRead;
+        private System.Windows.Forms.Button btnXMLWrite;
+        private System.Windows.Forms.Button btnXMLRead;
+        private System.Windows.Forms.Button btnSOAPWrite;
+        private System.Windows.Forms.Button btnSOAPRead;
+        private System.Windows.Forms.Button btnJSONWrite;
+        private System.Windows.Forms.Button btnJSONRead;
+    }
+}
+
---
+++ SerializationDemo/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text.Json;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializationDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBinaryWrite_Click(object sender, EventArgs e)
+        {
+          
+            try
+            {
+                Student stud = new Student();
+                stud.Id=Convert.ToInt32(txtId.Text);
+                stud.Name=txtName.Text;
+                stud.Percentage=Convert.ToDouble(txtPercentage.Text);
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studBinary.dat",FileMode.Create,FileAccess.Write);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, stud);
+                fs.Close();
+                MessageBox.Show("Record Added");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnBinaryRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studBinary.dat", FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                stud=(Student)bf.Deserialize(fs);
+                fs.Close ();
+                txtId.Text=stud.Id.ToString();
+                txtName.Text=stud.Name;
+                txtPercentage.Text=stud.Percentage.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXMLWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                stud.Id = Convert.ToInt32(txtId.Text);
+                stud.Name = txtName.Text;
+                stud.Percentage = Convert.ToDouble(txtPercentage.Text);
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studXML.xml", FileMode.Create, FileAccess.Write);
+                XmlSerializer xs= new XmlSerializer(typeof(Student));
+                xs.Serialize(fs, stud);
+                fs.Close();
+                MessageBox.Show("Record Added");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXMLRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studXML.xml", FileMode.Open, FileAccess.Read);
+               XmlSerializer xs=new XmlSerializer(typeof(Student));
+                stud = (Student)xs.Deserialize(fs);
+                fs.Close();
+                txtId.Text = stud.Id.ToString();
+                txtName.Text = stud.Name;
+                txtPercentage.Text = stud.Percentage.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSOAPWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                stud.Id = Convert.ToInt32(txtId.Text);
+                stud.Name = txtName.Text;
+                stud.Percentage = Convert.ToDouble(txtPercentage.Text);
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studSOAP.soap", FileMode.Create, FileAccess.Write);
+              SoapFormatter sf=new SoapFormatter();
+                sf.Serialize(fs, stud);
+                fs.Close();
+                MessageBox.Show("Record Added");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSOAPRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studSOAP.soap", FileMode.Open, FileAccess.Read);
+               SoapFormatter sf=new SoapFormatter();
+                stud = (Student)sf.Deserialize(fs);
+                fs.Close();
+                txtId.Text = stud.Id.ToString();
+                txtName.Text = stud.Name;
+                txtPercentage.Text = stud.Percentage.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJSONWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                stud.Id = Convert.ToInt32(txtId.Text);
+                stud.Name = txtName.Text;
+                stud.Percentage = Convert.ToDouble(txtPercentage.Text);
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studJSON.json", FileMode.Create, FileAccess.Write);
+               JsonSerializer.Serialize<Student>(fs, stud);
+                fs.Close();
+                MessageBox.Show("Record Added");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJSONRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stud = new Student();
+                FileStream fs = new FileStream(@"D:\.NET\Serialization\studJSON.json", FileMode.Open, FileAccess.Read);
+                stud=JsonSerializer.Deserialize<Student>(fs);
+                fs.Close();
+                txtId.Text = stud.Id.ToString();
+                txtName.Text = stud.Name;
+                txtPercentage.Text = stud.Percentage.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
